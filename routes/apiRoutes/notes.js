dfc2e616478986fd7393b60121faab93991292ed
Rachel Denton <rdenton3@terpmail.umd.edu>
2022-02-06 18:26:38 -0500

    api routes

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -1,4 +1,28 @@
 const router = require('express').Router(); 
+const { createNewNote } = require('../../lib/notes');
 const { notes } = require('../../db/db');
-// this is where the functions to handele the notes will go
-// keeping them separate from the routes and server info
+
+router.get('/notes', (req, res) => {
+    let results = notes;
+    // if (req.query) {
+    //   results = filterByQuery(req.query, results);
+    // }
+    res.json(results);
+  });
+
+router.post('/notes', (req, res) => {
+    // set id based on what the next index of the array will be
+    // req.body.id = animals.length.toString();
+
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+
+    // if (!validateAnimal(req.body)) {
+    //   res.status(400).send('The animal is not properly formatted.');
+    // } else {
+    //   const animal = createNewAnimal(req.body, animals);
+    //   res.json(animal);
+    // }
+  });
+  
+  module.exports = router;
